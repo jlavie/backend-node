4cc0e8d3a8502460edbb2b25323709a93e87e2e7
petitlardon <lavie.jul@gmail.com>
2025-01-15 18:51:11 +0100

    install nodemon for automatic server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const http = require('http');
+//install nodemon and run command to automatically run server
+// npm install -g nodemon 
+// nodemon server
 
 // a chaque requete sur le serveur, la fonction suivante sera appelée
 const server = http.createServer((req, res) => {
-    res.end('Voici la réponse')
+    res.end('Voici la réponse du serveur')
 })
 
 // ecoute sur le port déclaré dans l'app ou par défaut le 3000
