89467d868fa6b1709c7db91de8e18f7ab09aef03
petitlardon <lavie.jul@gmail.com>
2025-01-15 22:03:19 +0100

    utilisation d'express sur le serveur

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const http = require('http');
 // npm install -g nodemon 
 // nodemon server
 
+const app = require('./app');
+
+app.set('port', process.env.PORT || 3000);
+
 // a chaque requete sur le serveur, la fonction suivante sera appelée
-const server = http.createServer((req, res) => {
-    res.end('Voici la réponse du serveur')
-})
+// const server = http.createServer((req, res) => {
+//     res.end('Voici la réponse du serveur')
+// })
+const server = http.createServer(app);
 
 // ecoute sur le port déclaré dans l'app ou par défaut le 3000
 server.listen(process.env.PORT || 3000)
