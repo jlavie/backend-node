b5254c3890d9037677302fe49d12c4525eb982b2
petitlardon <lavie.jul@gmail.com>
2025-01-17 09:24:25 +0100

    creation methode express qui intercepte toutes les requetes

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ const express = require('express');
 // création de l'application express
 const app = express();
 
+app.use((req, res) => {
+    res.json({
+        messge: 'Requête bien reçue'
+    })
+})
+
 // exporter pour pouvoir y avoir accès, notemment le server node (server.js)
 module.exports = app;
