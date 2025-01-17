a49b6041e170eebdaedfb24e0195939e67caf6af
petitlardon <lavie.jul@gmail.com>
2025-01-17 19:32:46 +0100

    devlare express.json to get req.body + add post route

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 // création de l'application express
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -11,7 +13,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/api/stuff', (req, res, next) => {
+app.get('/api/stuff', (req, res, next) => {
     const stuff = [
       {
         _id: 'oeihfzeoi',
@@ -32,6 +34,13 @@ app.use('/api/stuff', (req, res, next) => {
     ];
     res.status(200).json(stuff);
   });
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Objet créé'
+    })
+});
 
 // exporter pour pouvoir y avoir accès, notemment le server node (server.js)
 module.exports = app;
