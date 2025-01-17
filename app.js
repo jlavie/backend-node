4eeef090764a08a641ff27a36cb3cbdab09b12e7
petitlardon <lavie.jul@gmail.com>
2025-01-17 17:53:15 +0100

    add endpoint to the middleware

--- app.js
+++ app.js
@@ -4,26 +4,27 @@ const express = require('express');
 // création de l'application express
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Requete reçue');
-    next();
-})
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-})
-
-app.use((req, res, next) => {
-    res.json({
-        messge: 'Requête bien reçue'
-    })
-    next();
-});
-
-app.use((req, res) => {
-  console.log('Réponse envoyée avec succès !');
-});
-
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
+  
 // exporter pour pouvoir y avoir accès, notemment le server node (server.js)
 module.exports = app;
