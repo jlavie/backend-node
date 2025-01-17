1aa959daa1a6bdb75ffc981baefcfd22b4a090cd
petitlardon <lavie.jul@gmail.com>
2025-01-17 22:47:29 +0100

    connect api to mongodb

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // importation d'express
 const express = require('express');
+const mongoose = require('mongoose');
 
 // création de l'application express
 const app = express();
+// mongoose.connect('mongodb://localhost:27017/dbconnect')
+mongoose.connect('mongodb://127.0.0.1:27017/dbconnect')
+.then(() => console.log('Connection à MongoDB réussie'))
+.catch(() => console.log('Connection à MongoDB échouée'))
+
+// mongoose.connection
+// .once('open', () => console.log('Connection à MongoDB réussie'))
+// .on('error', error => console.log('Connection à MongoDB échouée', error))
 
 app.use(express.json());
 
