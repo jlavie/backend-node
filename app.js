980bef9091e411e83ea1aff9bf8381c26d429e57
petitlardon <lavie.jul@gmail.com>
2025-01-18 00:46:01 +0100

    get one thing from database

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ app.use((req, res, next) => {
     next();
 });
 
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({_id: req.params.id})
+  .then(thing => res.status(200).json(thing))
+  .catch(error => res.status(404).json(error))
+});
+
 app.get('/api/stuff', (req, res, next) => {
   Thing.find()
   .then(things => res.status(200).json(things))
