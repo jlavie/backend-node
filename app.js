a5670d269f73c3b405892df8da789f699d70cfba
petitlardon <lavie.jul@gmail.com>
2025-01-17 18:51:07 +0100

    set CORS to synchronise backend with frontLa méthode app.use() vous permet d'attribuer un middleware à une route spécifique de votre application.
    Le CORS définit comment les serveurs et les navigateurs interagissent, en spécifiant quelles ressources peuvent être demandées de manière légitime – par défaut, les requêtes AJAX sont interdites.
    
    Pour permettre des requêtes cross-origin (et empêcher des erreurs CORS), des headers spécifiques de contrôle d'accès doivent être précisés pour tous vos objets de réponse.end

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const express = require('express');
 // création de l'application express
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
       {
@@ -25,6 +32,6 @@ app.use('/api/stuff', (req, res, next) => {
     ];
     res.status(200).json(stuff);
   });
-  
+
 // exporter pour pouvoir y avoir accès, notemment le server node (server.js)
 module.exports = app;
