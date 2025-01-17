6085fe29ccebdd54b83af878a4fbfeb34dcfaeb5
petitlardon <lavie.jul@gmail.com>
2025-01-17 14:34:37 +0100

    creation et utilisation de middleware

--- app.js
+++ app.js
@@ -4,11 +4,26 @@ const express = require('express');
 // création de l'application express
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+    console.log('Requete reçue');
+    next();
+})
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+})
+
+app.use((req, res, next) => {
     res.json({
         messge: 'Requête bien reçue'
     })
-})
+    next();
+});
+
+app.use((req, res) => {
+  console.log('Réponse envoyée avec succès !');
+});
 
 // exporter pour pouvoir y avoir accès, notemment le server node (server.js)
 module.exports = app;
