44873883d3424790ba020a0754a073d63a7a75d8
petitlardon <lavie.jul@gmail.com>
2025-01-17 23:18:29 +0100

    create data scheme

---
+++ models/things.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const thingSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        imageUrl: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        userId: {
+            type: String,
+            required: true,
+        },
+      }
+);
+
+const Thing = mongoose.model('Thing', thingSchema);
+module.exports = Thing;
