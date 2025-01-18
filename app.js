865a799393a90fc693933b06d81bf84790b9987e
petitlardon <lavie.jul@gmail.com>
2025-01-18 01:50:26 +0100

    delete thing in database

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ app.get('/api/stuff/:id', (req, res, next) => {
   .catch(error => res.status(404).json(error))
 });
 
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({_id: req.params.id})
+  .then(res.status(200).json({message: 'Objet supprimé'}))
+  .catch(error => res.status(404).json(error))
+});
+
 app.get('/api/stuff', (req, res, next) => {
   Thing.find()
   .then(things => res.status(200).json(things))
